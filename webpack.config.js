13f270fe1077ca1c8785e95ed11ebd7dc4c2162f
lxiang <carlosxl@gmail.com>
2016-09-29 22:32:20 +0800

    Initial project layout

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: "./src/app/entry.js",
+  output: {
+      path: __dirname + '/build',
+      filename: "app.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  }
+};
