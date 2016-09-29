50fb84df85c457e1ad3b263d525cea6042c4dcb9
lxiang <carlosxl@gmail.com>
2016-09-30 00:39:32 +0800

    game engine wip

--- src/app/entry.js
+++ src/app/entry.js
@@ -3,4 +3,18 @@ var Game = require('../engine/game');
 document.write("hello world!");
 
 var game = new Game();
-game.log();
+
+game.move(0);
+game.move(1);
+game.move(2);
+game.move(3);
+game.move(4);
+game.move(5);
+game.move(6);
+game.move(0);
+game.move(1);
+game.move(2);
+game.move(3);
+game.move(4);
+game.move(5);
+game.move(6);
--- src/engine/game.js
+++ src/engine/game.js
@@ -2,12 +2,55 @@ var _ = require('lodash');
 
 class Game {
   constructor() {
+    // [Array(6) x 7], bottom left corner is board[0][0], upper left is board[0][5]
     this.board = _.chunk(Array(6 * 7), 6);
+
+    this.last_move = null;
+    this.next_disc = Game.DISC1;
+    this.total_moves = 0;
   }
 
-  log {
-    console.log(this.board);
+  switch_player() {
+    this.next_disc = this.next_disc ^ 1;
+  }
+
+  move(colN) {
+    if (this.is_end_game()) {
+      this.draw();
+      return
+    }
+
+    var col = this.board[colN];
+    col[_.indexOf(col, undefined)] = this.next_disc;
+    this.switch_player();
+    this.draw();
+  }
+
+  is_end_game() {
+    return false;
+  }
+
+  draw() {
+    var self = this;
+    console.log('  ______________')
+    _.forEach(_.range(5, -1, -1), function(rowN) {
+      var row = rowN + ': |';
+      _.forEach(_.range(6), function(colN) {
+        var _map = {
+          undefined: ' ',
+          1: 'X',
+          0: 'O',
+        }
+        row += (_map[self.board[colN][rowN]]) + '|';
+      });
+      console.log(row);
+    });
+    console.log('  ______________')
   }
 }
+
+// Static data properties
+Game.DISC1 = 0;
+Game.DISC2 = 1;
 
 module.exports = Game;
